06c2c88e36d3df58b4e1423a353f727750d61042
Erick Santiago Amezcua Tarango <orick08@Ericks-MacBook-Pro.local>
2022-03-02 13:05:47 -0700

    Init commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using joke.Models;
+
+namespace jokes.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<joke.Models.Joke> Joke { get; set; }
+}
